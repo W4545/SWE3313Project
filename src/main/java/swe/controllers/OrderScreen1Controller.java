390bb8854f6981d4bc2fef6b2a4c76765a11e9b6
jacky <jackyoung@lost.parts>
2020-04-13 20:04:07 -0400

    Added some controls to the OrderScreens

--- src/main/java/swe/controllers/OrderScreen1Controller.java
+++ src/main/java/swe/controllers/OrderScreen1Controller.java
@@ -3,23 +3,97 @@ package swe.controllers;
 // Name: Jack Young
 // Date: 4/13/2020
 
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.VBox;
 import swe.launch.App;
 
-public class OrderScreen1Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class OrderScreen1Controller implements Initializable {
 
 	private App app;
 
 	@FXML
 	private ToggleGroup crustToggleGroup;
 
+	@FXML
+	private CheckBox smallCheckBox;
+
+	@FXML
+	private CheckBox mediumCheckBox;
+
+	@FXML
+	private CheckBox largeCheckBox;
+
+	@FXML
+	private VBox drinkBox;
+
+	private final Property<Boolean> disableDrinkBox = new SimpleBooleanProperty(true);
+
+	private final Property<Boolean> disableSmall = new SimpleBooleanProperty(false);
+
+	private final Property<Boolean> disableMedium = new SimpleBooleanProperty(false);
+
+	private Property<Boolean> disableLarge = new SimpleBooleanProperty(false);
+
+	private char selectedSize;
+
 	public void setApp(App app) {
 		this.app = app;
 	}
 
 	@FXML
+	void small() {
+		disableDrinkBox.setValue(!disableDrinkBox.getValue());
+		if (!disableDrinkBox.getValue()) {
+			disableMedium.setValue(true);
+			disableLarge.setValue(true);
+		} else {
+			disableMedium.setValue(false);
+			disableLarge.setValue(false);
+		}
+	}
+
+	@FXML
+	void medium() {
+		disableDrinkBox.setValue(!disableDrinkBox.getValue());
+		if (!disableDrinkBox.getValue()) {
+			disableSmall.setValue(true);
+			disableLarge.setValue(true);
+		} else {
+			disableSmall.setValue(false);
+			disableLarge.setValue(false);
+		}
+	}
+
+	@FXML
+	void large() {
+		disableDrinkBox.setValue(!disableDrinkBox.getValue());
+		if (!disableDrinkBox.getValue()) {
+			disableSmall.setValue(true);
+			disableMedium.setValue(true);
+		} else {
+			disableSmall.setValue(false);
+			disableMedium.setValue(false);
+		}
+	}
+
+	@FXML
 	private void nextButton() {
 		app.getOrderScreenController().setRight(app.getOrderScreen2());
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		drinkBox.disableProperty().bind(disableDrinkBox);
+		smallCheckBox.disableProperty().bind(disableSmall);
+		mediumCheckBox.disableProperty().bind(disableMedium);
+		largeCheckBox.disableProperty().bind(disableLarge);
 	}
 }
--- src/main/java/swe/controllers/OrderScreenController.java
+++ src/main/java/swe/controllers/OrderScreenController.java
@@ -3,12 +3,22 @@ package swe.controllers;
 // Name: Jack Young
 // Date: 4/12/2020
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.SplitPane;
 import swe.launch.App;
 
-public class OrderScreenController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.function.Consumer;
+
+public class OrderScreenController implements Initializable {
 
 	private App app;
 
@@ -24,5 +34,11 @@ public class OrderScreenController {
 		if (splitPane.getItems().size() > 1)
 			splitPane.getItems().remove(1);
 		splitPane.getItems().add(parent);
+		splitPane.setDividerPositions(.25);
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
 	}
 }
